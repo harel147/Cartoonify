82a197a1326af24552d49eb4b6bd136c0de91139
Harel <harel@lynxight.com>
2023-06-01 20:35:11 +0300

    after cartoon to dataset

--- CartoonGAN_for_torch/test.py
+++ CartoonGAN_for_torch/test.py
@@ -44,12 +44,12 @@ for files in os.listdir(opt.input_dir):
 	h = input_image.size[0]
 	w = input_image.size[1]
 	ratio = h *1.0 / w
-	if ratio > 1:
-		h = opt.load_size
-		w = int(h*1.0/ratio)
-	else:
-		w = opt.load_size
-		h = int(w * ratio)
+	# if ratio > 1:
+	# 	h = opt.load_size
+	# 	w = int(h*1.0/ratio)
+	# else:
+	# 	w = opt.load_size
+	# 	h = int(w * ratio)
 	input_image = input_image.resize((h, w), Image.BICUBIC)
 	input_image = np.asarray(input_image)
 	# RGB -> BGR
--- train_facial_expression.py
+++ train_facial_expression.py
@@ -1,2 +1,75 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torchvision import datasets, models, transforms
 
-# TODO: THE ENTIRE PROJECT
+# Set device
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Define data transformations
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Load FER2013 dataset
+train_data = datasets.ImageFolder("./FER2013/train", transform=transform)
+test_data = datasets.ImageFolder("./FER2013/test", transform=transform)
+
+# Create data loaders
+batch_size = 64
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
+
+# Load pre-trained ResNet-18 model
+model = models.resnet18(pretrained=True)
+num_classes = len(train_data.classes)
+model.fc = nn.Linear(512, num_classes)  # Adjust the last fully connected layer for the correct number of classes
+
+# Move model to device
+model = model.to(device)
+
+# Define loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training loop
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+
+    for images, labels in train_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {running_loss / len(train_loader):.4f}")
+
+# Evaluation
+model.eval()
+correct = 0
+total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+accuracy = correct / total
+print(f"Test Accuracy: {accuracy:.4f}")
